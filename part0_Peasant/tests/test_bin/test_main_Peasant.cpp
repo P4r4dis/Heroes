6377c5e467fba6aa797bc829af98a7073996f537
P4r4dis <adil.d.pro@gmail.com>
2023-03-03 18:58:50 +0100

    [ADD] - unit test for Peasant construction add

--- part0_Peasant/tests/test_bin/test_main_Peasant.cpp
+++ part0_Peasant/tests/test_bin/test_main_Peasant.cpp
@@ -3,7 +3,9 @@
 #include <criterion/redirect.h>
 #include <criterion/logging.h>
 #include <criterion/parameterized.h>
-#include <criterion/redirect.h>
+#include <signal.h>
+
+#include "../test_include/test_Peasant.hpp"
 
 void    redirect_all_stdout(void)
 {
@@ -11,7 +13,32 @@ void    redirect_all_stdout(void)
     cr_redirect_stderr();
 }
 
-Test(Peasant, test_construction, .init = redirect_all_stdout)
+Test(Peasant, test_construction, .signal = SIGPIPE, .init = redirect_all_stdout)
 {
-    // cr_assert_stdout_eq_str("TEST\n");
+    Peasant     peasant("Gildas", 42);
+
+    cr_assert(peasant.getName() == "Gildas");
+    cr_assert(peasant.getPower() == 42);
+    peasant.setPower(150);
+    cr_assert(peasant.getPower() == 100);
+    cr_assert(peasant.getHp() == 100);
+    peasant.setHp(150);
+    cr_assert(peasant.getHp() == 100);
+    cr_assert(peasant.getCostAttack() == 10);
+    peasant.setCostAttack(15);
+    cr_assert(peasant.getCostAttack() == 15);
+    cr_assert(peasant.getCostSpecial() == 0);
+    peasant.setCostSpecial(10);
+    cr_assert(peasant.getCostSpecial() == 10);
+    cr_assert(peasant.getCostRest() == 0);
+    peasant.setCostRest(10);
+    cr_assert(peasant.getCostRest() == 10);
+    cr_assert(peasant.getDamageAttack() == 5);
+    peasant.setDamageAttack(10);
+    cr_assert(peasant.getDamageAttack() == 10);
+    cr_assert(peasant.getDamageSpecial() == 0);
+    peasant.setDamageSpecial(10);
+    cr_assert(peasant.getDamageSpecial() == 10);
+    peasant.~Peasant();
+    cr_assert_stdout_eq_str("Gildas goes for an adventure.\nGildas is back to his crops.\n");
 }
--- part0_Peasant/tests/test_src/test_Peasant.cpp
+++ part0_Peasant/tests/test_src/test_Peasant.cpp
@@ -1,1 +1,89 @@
 #include "../test_include/test_Peasant.hpp"
+
+Peasant::Peasant(const std::string &name, int power)
+    :   m_name(name), m_power((power > 100) ? power = 100 : power),
+        m_hp(100), m_costAttack(10), m_costSpecial(0), m_costRest(0),
+        m_damageAttack(5), m_damageSpecial(0)
+{
+    std::cout << m_name + " goes for an adventure." << std::endl;
+}
+
+Peasant::~Peasant(void)
+{
+    std::cout << m_name + " is back to his crops." << std::endl;
+}
+
+const std::string       &Peasant::getName(void) const
+{
+    return m_name;
+}
+
+int                     Peasant::getPower(void) const
+{
+    return m_power;
+}
+
+int                     Peasant::getHp(void) const
+{
+    return m_hp;
+}
+
+int                     Peasant::getCostAttack(void) const
+{
+    return m_costAttack;
+}
+
+int                     Peasant::getCostSpecial(void) const
+{
+    return m_costSpecial;
+}
+
+int                     Peasant::getCostRest(void) const
+{
+    return m_costRest;
+}
+
+int                     Peasant::getDamageAttack(void) const
+{
+    return m_damageAttack;
+}
+
+int                     Peasant::getDamageSpecial(void) const
+{
+    return m_damageSpecial;
+}
+
+void                    Peasant::setPower(int power)
+{
+    (power > 100) ? m_power = 100 : m_power = power;
+}
+
+void                    Peasant::setHp(int hp)
+{
+    (hp > 100) ? m_hp = 100 : m_hp = hp;
+}
+
+void                    Peasant::setCostAttack(int costAttack)
+{
+    m_costAttack = costAttack;
+}
+
+void                    Peasant::setCostSpecial(int costSpecial)
+{
+    m_costSpecial = costSpecial;
+}
+
+void                    Peasant::setCostRest(int costRest)
+{
+    m_costRest = costRest;
+}
+
+void                    Peasant::setDamageAttack(int damageAttack)
+{
+    m_damageAttack = damageAttack;
+}
+
+void                    Peasant::setDamageSpecial(int damageSpecial)
+{
+    m_damageSpecial = damageSpecial;
+}
