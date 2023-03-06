b3ba537b2c66252b1036671cc483572789f4e2d9
P4r4dis <adil.d.pro@gmail.com>
2023-03-06 17:37:35 +0100

    [ADD] - unit test for Peasant::damage add

--- part0_Peasant/tests/test_bin/test_main_Peasant.cpp
+++ part0_Peasant/tests/test_bin/test_main_Peasant.cpp
@@ -94,3 +94,20 @@ Test(Peasant, test_rest, .signal = SIGPIPE, .init = redirect_all_stdout)
     "Gildas takes a nap.\n"
     "Gildas is back to his crops.\n");
 }
+
+Test(Peasant, test_damage, .signal = SIGPIPE, .init = redirect_all_stdout)
+{
+    Peasant     peasant("Gildas", 100);
+
+    cr_assert(peasant.getHp() == 100);
+    peasant.damage(50);
+    cr_assert(peasant.getHp() == 50);
+    peasant.damage(100);
+    cr_assert(peasant.getHp() == 0);
+
+    peasant.~Peasant();
+    cr_assert_stdout_eq_str("Gildas goes for an adventure.\n"
+    "Gildas takes 50 damage.\n"
+    "Gildas is out of combat.\n"
+    "Gildas is back to his crops.\n");
+}
