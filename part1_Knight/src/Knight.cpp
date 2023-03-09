f83ad6145d4b3f5fe85d441ce322dfd4f4f5d268
P4r4dis <adil.d.pro@gmail.com>
2023-03-09 18:20:23 +0100

    [ADD] - feature Knight::~Knight add

--- part1_Knight/src/Knight.cpp
+++ part1_Knight/src/Knight.cpp
@@ -5,6 +5,11 @@ Knight::Knight(const std::string &name, int power) : Peasant(name, power)
     std::cout << getName() << " vows to protect the kingdom." << std::endl;
 }
 
+Knight::~Knight()
+{
+    std::cout << getName() << " takes off his armor." << std::endl;
+}
+
 int                     Knight::attack()
 {
     int damage = getDamageAttack();
