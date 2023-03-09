f2b322cf8f79b45e8d979b1baebf8ab917cbcfbb
P4r4dis <adil.d.pro@gmail.com>
2023-03-09 18:30:05 +0100

    [ADD] - feature Knight main function add

--- part1_Knight/bin/main_Knight.cpp
+++ part1_Knight/bin/main_Knight.cpp
@@ -2,11 +2,19 @@
 
 int main()
 {
-    std::cout << "REAL MAIN STARTING :" << std::endl;
     Knight      k("Thrall", 40);
 
     k.attack();
     k.special();
     k.rest();
+    std::cout << "REAL MAIN STARTING :" << std::endl;
+
+    Knight      knight("Arthur", 20);
+
+    knight.attack();
+    knight.special();
+    knight.rest();
+    knight.special();
+    knight.damage(50);
     return 0;
 }
