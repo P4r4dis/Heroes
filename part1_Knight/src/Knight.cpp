9334fa5ad5d1804b9ddca97adce8d7e9a7f11c35
P4r4dis <adil.d.pro@gmail.com>
2023-03-09 14:23:54 +0100

    [UPDATE] - Knight construction update

--- part1_Knight/src/Knight.cpp
+++ part1_Knight/src/Knight.cpp
@@ -1,7 +1,9 @@
 #include "../include/Knight.hpp"
 
 Knight::Knight(const std::string &name, int power) : Peasant(name, power)
-{}
+{
+    std::cout << getName() << " vows to protect the kingdom." << std::endl;
+}
 
 int                     Knight::attack()
 {
